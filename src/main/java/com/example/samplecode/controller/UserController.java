c2b2782cc2db9dab57fdc8e75ea549d080d29cea
chipchippp <114909102+chipchippp@users.noreply.github.com>
2025-02-20 21:45:56 +0700

    lombok

--- src/main/java/com/example/samplecode/controller/UserController.java
+++ src/main/java/com/example/samplecode/controller/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +18,8 @@ import java.util.List;
 @RequestMapping("/api/v1/user")
 @Slf4j
 public class UserController {
-    private UserService userService;
+//    @Autowired
+    private final UserService userService;
 
 
     @GetMapping("/list")
@@ -25,7 +27,7 @@ public class UserController {
     public ResponseData<List<UserRequestDTO>> getUsers(
             @RequestParam(defaultValue = "0", required = false) int pageNo,
             @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
-        System.out.println("Get all users");
+        log.info("Get all users");
         return new ResponseData<>(HttpStatus.OK.value(), "User found", List.of(
                 new UserRequestDTO("John" , "Doe", "john@gmail.com", "1234567890"),
                 new UserRequestDTO("Jane", "Doe", "jane@gmail.com", "0987654321")));
@@ -34,15 +36,15 @@ public class UserController {
     @GetMapping("/{userId}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseData<UserRequestDTO> getUser(@PathVariable @Min(1) Long userId) {
-        System.out.println("User id: " + userId);
+        log.info("User id: " + userId);
         return new ResponseData<>(HttpStatus.OK.value(), "User found", new UserRequestDTO("John" , "Doe", "john@gmail.com", "1234567890"));
     }
 
     @PostMapping("/add")
     public ResponseData<Integer> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
-        System.out.println("Request: " + userRequestDTO.getFirstName());
+        log.info("Request add user = {} {}: " + userRequestDTO.getFirstName(), userRequestDTO.getLastName());
         try {
-            userService.addUser(userRequestDTO);
+//            userService.addUser(userRequestDTO);
             return new ResponseData<>(HttpStatus.CREATED.value(), "User created successful");
         } catch (Exception e) {
             return new ResponseData<>(HttpStatus.BAD_REQUEST.value(), "save creation failed!!!");
@@ -52,22 +54,21 @@ public class UserController {
     @PutMapping("/{userId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseData<?> updateUser(@PathVariable @Min(1) Long userId, @Valid @RequestBody UserRequestDTO userRequestDTO) {
-        System.out.println("User id: " + userId);
+        log.info("User id = {} ", userId);
         return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User updated successful");
     }
 
     @PatchMapping("/{userId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseData<?> changeStatus(@Min(1) @PathVariable Long userId, @RequestParam(value = "status", required = false) boolean status) {
-        System.out.println("User id: " + userId);
-        System.out.println("Status: " + status);
+        log.info("User id = {} ", userId);
         return new ResponseData<>(HttpStatus.ACCEPTED.value(), "Status changed successful");
     }
 
     @DeleteMapping("/{userId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseData<?> deleteUser(@PathVariable("userId") @Min(value = 1, message = "userId must be greater than 0") Long id) {
-        System.out.println("User id: " + id);
+        log.info("User id = {} ", id);
         return new ResponseData<>(HttpStatus.NO_CONTENT.value(), "User deleted successful");
     }
 }
